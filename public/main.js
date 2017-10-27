d7a58522cc67766b24657e7bceda6518b2df5239
blakethomas <blakekthomas49@gmail.com>
2017-10-27 09:44:33 -0700

    Fixes whitespace with prettier package.

--- public/main.js
+++ public/main.js
@@ -4,39 +4,35 @@ const $btnBottomRight = document.querySelector('.bottom-right')
 const $btnBottomLeft = document.querySelector('.bottom-left')
 const $btnPuig = document.querySelector('.puig')
 const $buttons = [
-$btnTopRight,
-$btnTopLeft,
-$btnBottomRight,
-$btnBottomLeft,
-$btnPuig
+  $btnTopRight,
+  $btnTopLeft,
+  $btnBottomRight,
+  $btnBottomLeft,
+  $btnPuig
 ]
-
 
 const locationData = []
 
 $buttons.forEach(button => {
-  const {top, right, left, bottom} = button.getBoundingClientRect()
-  locationData.push({top, right, left, bottom})
+  const { top, right, left, bottom } = button.getBoundingClientRect()
+  locationData.push({ top, right, left, bottom })
 })
 
-for(let i = 0; i < locationData.length; i++){
+for (let i = 0; i < locationData.length; i++) {
   const distanceTop = window.innerHeight - locationData[i].top
   const distanceRight = window.innerWidth - locationData[i].right
   const distanceLeft = window.innerWidth - locationData[i].left
   const distanceBottom = window.innerHeight - locationData[i].bottom
-  if(distanceRight >= 100){
+  if (distanceRight >= 100) {
     $buttons[i].classList.add('right')
     $buttons[i].classList.remove('top', 'bottom', 'left')
-  }
-  else if(distanceBottom >= 20){
+  } else if (distanceBottom >= 20) {
     $buttons[i].classList.add('bottom')
     $buttons[i].classList.remove('top', 'right', 'left')
-  }
-  else if(distanceTop >= 20){
+  } else if (distanceTop >= 20) {
     $buttons[i].classList.add('top')
     $buttons[i].classList.remove('bottom', 'right', 'left')
-  }
-  else {
+  } else {
     $buttons[i].classList.add('left')
     $buttons[i].classList.remove('top', 'right', 'bottom')
   }
